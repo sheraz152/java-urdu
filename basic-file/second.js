f1dfe12da857ee93a61f7a055f0b9b6e98227d61
sheraz152 <sdd18436@gmail.com>
2024-09-20 05:30:33 +0000

    some tools

---
+++ basic-file/second.js
@@ -0,0 +1,19 @@
+const accountId=2134
+let accountEmail="hc@hc.com"
+let accountPassward="453"
+accountState="pakistan"
+accountCITY="Multan"
+
+console.table( [accountId, accountEmail,accountPassward, accountState,accountCITY] )
+
+
+
+
+let age=20
+
+
+console.log(typeof undefined)
+
+
+
+
