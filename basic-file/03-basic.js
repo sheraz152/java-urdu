0943274cd4dac4957c8d329601bfef64414af346
sheraz152 <sdd18436@gmail.com>
2024-09-22 04:12:40 +0000

    conversion

---
+++ basic-file/03-basic.js
@@ -0,0 +1,23 @@
+//let score=true
+//console.log(typeof score);
+
+//let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//let accountId=3424
+//let accountEmail="hs@gmail.com"
+//accountCity="multan"
+//accountState="pakistan"
+//console.table( [accountId, accountEmail, accountCity, accountState] )
+
+
+//let IsLoggedIn=0
+//let booleanIsLoggedIn=Boolean(IsLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let AnyNumber=55
+let stringAnyNumber=String(AnyNumber)
+console.log(stringAnyNumber)
+console.log(typeof stringAnyNumber)
